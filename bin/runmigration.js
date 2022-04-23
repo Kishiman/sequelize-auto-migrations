9224adb327b5cdcb271197f0cee2e0a0ce08e6f3
Kishiman <fares.manaisademx@gmail.com>
2022-04-23 15:48:32 +0100

    1.3.0

--- bin/runmigration.js
+++ bin/runmigration.js
@@ -93,7 +93,7 @@ async function executeSql(queryInterface, sql) {
   //   'CREATE TABLE IF NOT EXISTS `SequelizeMeta` (`name` varchar(50) UNIQUE)'
   // );
   const sequelize = queryInterface.sequelize
-  queryInterface.queryGenerator = new sequelize.dialect.QueryGenerator({ sequelize, _dialect: sequelize.dialect })
+  queryInterface.queryGenerator = queryInterface.queryGenerator || new sequelize.dialect.QueryGenerator({ sequelize, _dialect: sequelize.dialect })
   const SequelizeMeta = sequelize.define("SequelizeMeta", { name: { type: sequelize.Sequelize.DataTypes.STRING(50), primaryKey: true } }, { timestamps: false, paranoid: false })
   let createIfNot = await SequelizeMeta.sync()
   // let res = await executeSql(queryInterface, 'select * from `SequelizeMeta`');
